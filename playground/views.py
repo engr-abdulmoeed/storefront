1ac5d4247380b97f8b12cafd758ac6c3d1ebe2ae
Abdul Moeed <getmoeed@gmail.com>
2024-03-18 23:33:42 +0500

    initial commit added models for storefront added migrations

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from store.models import Product
+
+
+def say_hello(request):
+    queryset = Product.objects.filter(title__icontains='coffee')
+    
+    return render(request, 'hello.html', {'name': 'Moeed', 'products': list(queryset)})
