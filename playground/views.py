bea03d4d31c2969f33609b5d63332ff745467e1e
Abdul Moeed <getmoeed@gmail.com>
2024-03-19 00:22:33 +0500

    using Q objects to filter data

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
 from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(title__icontains='coffee')
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     
     return render(request, 'hello.html', {'name': 'Moeed', 'products': list(queryset)})
