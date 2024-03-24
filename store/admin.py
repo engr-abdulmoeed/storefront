920b119fd8ef3e08c519b53a52d3c115b7cab417
Abdul Moeed <getmoeed@gmail.com>
2024-03-24 23:18:04 +0500

    customizing list page in django admin site

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from store.models import Collection, Product
+from store.models import Collection, Product, Customer
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
 
 
 admin.site.register(Collection)
-admin.site.register(Product)
