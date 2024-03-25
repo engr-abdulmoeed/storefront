0fcba65774d3fc35535c63cfb1560ba0b87a1627
Abdul Moeed <getmoeed@gmail.com>
2024-03-25 23:06:18 +0500

    overriding the base queryset in django admin site

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from store.models import Collection, Product, Customer
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from store.models import Collection, Product, Customer, Order
+from django.db.models import Count
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        else:
+            return 'OK'
 
 
 @admin.register(Customer)
@@ -15,4 +32,18 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
 
 
-admin.site.register(Collection)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Collection]:
+        return super().get_queryset(request).annotate(products_count=Count('product'))
