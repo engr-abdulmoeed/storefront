4b1cab34c4ce8e4e8d4e9c36d28dc813e8632cca
Abdul Moeed <getmoeed@gmail.com>
2024-04-06 01:06:25 +0500

    implementing hyperlink for related fields using Serializers

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from store.views import product_list, product_detail
+from store.views import product_list, product_detail, collection_detail
 
 
 urlpatterns = [
     path('product/', product_list),
-    path('product/<int:id>/', product_detail)
+    path('product/<int:id>/', product_detail),
+    path('collection/<int:pk>/', collection_detail, name='collection-detail')
 ]
--- store/views.py
+++ store/views.py
@@ -1,15 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from store.models import Product
-from store.serializers import ProductSerializer
-from rest_framework import status
+from store.models import Product, Collection
+from store.serializers import ProductSerializer, CollectionSerialzier
 from django.shortcuts import get_object_or_404
 
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.all().select_related('collection')
+    serializer = ProductSerializer(
+        queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
@@ -17,4 +17,11 @@ def product_list(request):
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerialzier(collection)
     return Response(serializer.data)
