127944a63d894748ed39c45121f11a3f8e6051f1
Abdul Moeed <getmoeed@gmail.com>
2024-04-28 01:46:05 +0500

    using nested routers to show each products' reviews

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
-from store.views import ProductViewSet, CollectionViewSet
-from rest_framework.routers import DefaultRouter
+from store.views import ProductViewSet, CollectionViewSet, ReviewViewSet
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('collections', CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', ReviewViewSet, basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
