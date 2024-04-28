0f2b1519b51232e3677c2a37a374b2db08d4b5c4
Abdul Moeed <getmoeed@gmail.com>
2024-04-28 22:41:37 +0500

    using generic filter using django-filter library to filter products by category and unit_price

--- store/views.py
+++ store/views.py
@@ -1,22 +1,20 @@
 from django.http import HttpRequest
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from store.filters import ProductFilter
 from store.models import OrderItem, Product, Collection, Review
 from store.serializers import ProductSerializer, CollectionSerialzier, ReviewSerializer
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from django.db.models import Count
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id', None)
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
