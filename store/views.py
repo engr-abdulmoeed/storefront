abcf0a98adeda1bdee8397ec1bc9e6ad26980c37
Abdul Moeed <getmoeed@gmail.com>
2024-04-28 23:00:35 +0500

    implementing pagination on Products end point

--- store/views.py
+++ store/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Count
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from store.filters import ProductFilter
 from store.models import OrderItem, Product, Collection, Review
+from store.pagination import DefaultPagination
 from store.serializers import ProductSerializer, CollectionSerialzier, ReviewSerializer
-from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
-from django.db.models import Count
 
 
 class ProductViewSet(ModelViewSet):
@@ -16,6 +17,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
