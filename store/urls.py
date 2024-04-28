e16cb9b1f5d52838aad3ac8ed4016c84199d3f70
Abdul Moeed <getmoeed@gmail.com>
2024-04-28 22:26:08 +0500

    using query_params to filter products based on collection_id

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
+router.register('products', ProductViewSet, basename='products')
 router.register('collections', CollectionViewSet)
 
 products_router = NestedDefaultRouter(router, 'products', lookup='product')
